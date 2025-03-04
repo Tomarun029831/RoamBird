d8492dde143650d0a4c03eb22d048841c40fe2ca
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-03-04 16:40:44 +0900

    Implement of TurnPad

--- Gimmick/ConcreteGimmick/TurnPad.cs
+++ Gimmick/ConcreteGimmick/TurnPad.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class TurnPad : MonoBehaviour, Gimmick
 {
-    [SerializeField] private TurnPadScriptableObjectScript TurnPadScriptableObjectScript;
+    [SerializeField] private TurnPadScriptableObjectScript turnPadData;
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        Rigidbody2D rg = collision2D.rigidbody;
+        float direction = System.Math.Sign(collision2D.transform.position.x - gameObject.transform.position.x);
+        rg.linearVelocity = direction * turnPadData.KnockBackVelocity;
+    }
 }
