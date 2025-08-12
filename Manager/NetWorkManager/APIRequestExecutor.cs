8f64fc3800cb8acb0380b3776608418e4dfb3183
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-12 15:28:28 +0900

    fix(API)!: return text that is not destoried

--- Manager/NetWorkManager/APIRequestExecutor.cs
+++ Manager/NetWorkManager/APIRequestExecutor.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 public static class APIRequestExecutor
 {
-    public static async Task<(bool isSuccess, UnityWebRequest response)> PostJson(string url, string payload)
+    public static async Task<(bool isSuccess, string receivedPayloadText)> PostJson(string url, string payload)
     {
         Debug.Log("PostJson called with " + payload);
         byte[] jsonBytes = Encoding.UTF8.GetBytes(payload);
@@ -21,7 +21,7 @@ public static class APIRequestExecutor
             if (req.result != UnityWebRequest.Result.Success)
                 return (false, null);
 
-            return (true, req);
+            return (true, req.downloadHandler.text);
         }
     }
 }
