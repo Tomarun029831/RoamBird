64f9e14ce0aa84b6ac297e64d8d9ca244849cbbd
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-13 16:04:28 +0900

    chore(API)!: rename UserAPIClient to AccountAPIClient

--- Manager/NetWorkManager/AccountAPIClient.cs
+++ Manager/NetWorkManager/AccountAPIClient.cs
@@ -1,11 +1,11 @@
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-public static class UserAPIClient
+public static class AccountAPIClient
 {
     private const string APIURL = ENV.TOMATECHAPI;
 
-    public static async Task<(bool isSuccess, string token)> DoLogin(string plainUsername, string plainPassword)
+    public static async Task<bool> DoLogin(string plainUsername, string plainPassword)
     {
         const string mode = "AUTHENTICATE";
         string hashedPassword = ENV.ComputeHash(plainPassword);
@@ -18,20 +18,21 @@ public static class UserAPIClient
         string stringfiedObj = JsonConvert.SerializeObject(jsonObj);
 
         (bool isSuccess, string receivedPayload) = await APIRequestExecutor.PostJson(url: APIURL, payload: stringfiedObj);
-        if (!isSuccess) return (false, null);
+        if (!isSuccess) return false;
 
         ResponseToAccount responseJsonObj = null;
         try { responseJsonObj = JsonConvert.DeserializeObject<ResponseToAccount>(receivedPayload); }
-        catch (System.Exception) { return (false, null); }
-        if (responseJsonObj == null) return (false, null);
+        catch (System.Exception) { return false; }
+        if (responseJsonObj == null) return false;
 
         bool apiSuccess = responseJsonObj.result == "success";
         string token = responseJsonObj.payload;
+        TrackerAPIClient.SetToken(token);
 
-        return (apiSuccess, token);
+        return apiSuccess;
     }
 
-    public static async Task<(bool isSuccess, string token)> CreateAcconut(string plainUsername, string plainPassword)
+    public static async Task<bool> CreateAcconut(string plainUsername, string plainPassword)
     {
         const string mode = "CREATE";
         string hashedPassword = ENV.ComputeHash(plainPassword);
@@ -44,16 +45,17 @@ public static class UserAPIClient
         string stringfiedObj = JsonConvert.SerializeObject(jsonObj);
 
         (bool isSuccess, string receivedPayload) = await APIRequestExecutor.PostJson(url: APIURL, payload: stringfiedObj);
-        if (!isSuccess) return (false, null);
+        if (!isSuccess) return false;
         ResponseToAccount responseJsonObj = null;
         try { responseJsonObj = JsonConvert.DeserializeObject<ResponseToAccount>(receivedPayload); }
-        catch (System.Exception) { return (false, null); }
-        if (responseJsonObj == null) return (false, null);
+        catch (System.Exception) { return false; }
+        if (responseJsonObj == null) return false;
 
         bool apiSuccess = responseJsonObj.result == "success";
         string token = responseJsonObj.payload;
+        TrackerAPIClient.SetToken(token);
 
-        return (apiSuccess, token);
+        return apiSuccess;
     }
 
 }
