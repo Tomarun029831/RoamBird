26968327549a317d887e80ec73546b498b6a3659
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-08-15 22:48:46 +0900

    chore(Gimmick): add comment about performance

--- Gimmick/ConcreteGimmick/TurnPad.cs
+++ Gimmick/ConcreteGimmick/TurnPad.cs
@@ -6,7 +6,7 @@ public class TurnPad : MonoBehaviour, Gimmick
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {
-        collision2D.gameObject.GetComponent<Playable>().FlipX();
+        collision2D.gameObject.GetComponent<Playable>().FlipX(); // PERF: GetComponent is liner search => slow
         Rigidbody2D rg = collision2D.rigidbody;
         float direction = System.Math.Sign(collision2D.transform.position.x - gameObject.transform.position.x);
         rg.linearVelocity = direction * turnPadData.KnockBackVelocity;
